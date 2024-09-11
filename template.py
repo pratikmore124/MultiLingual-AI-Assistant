d7e667fc4715209cb6606fdc590b55a7b686b8e8
pratikmore124 <pratikmore124@gmail.com>
2024-09-11 11:14:32 +0530

    update template

--- template.py
+++ template.py
@@ -1,45 +1,34 @@
 import os
-from pathlib import Path
 import logging
+from pathlib import Path
 
-logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "Multilongual-Assistant"
-
-list_of_file=[
-    ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logging/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py", 
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
-    "params.yaml",
-    "app.py",
-    "main.py",
-    "Dockerfile",
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
     "requirements.txt",
     "setup.py",
+    "app.py",
     "research/trials.ipynb"
 ]
 
 
-for filepath in list_of_file:
-    file_path = Path(filepath)
-    filedir,filename = os.path.split(file_path)
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir!="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for the file:{filename}")
 
-    if(not os.path.exists(file_path) or (os.path.getsize(file_path)) == 0):
-        with open(file_path,"w") as f:
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file:{file_path}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(f"{filename} already exist.")
+        logging.info(f"{filename} is already exists")
